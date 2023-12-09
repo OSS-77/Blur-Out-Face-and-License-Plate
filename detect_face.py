5af20ad493e193060a9286e9da460a1806be84f2
seock1000 <tjrcjs0705@gmail.com>
2023-12-09 14:33:45 +0900

    implement face detection

---
+++ detect_face.py
@@ -0,0 +1,17 @@
+import cv2
+
+def detectFace(imagePath) :
+    cascPath = "haarcascade_frontalface_default.xml"
+
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5
+    )
+    
+    return faces

819ba1352ac2b3b18cf8fc876f10e76ec3c4e739
yoonchulShin <145009711+yoonchulShin@users.noreply.github.com>
2023-12-12 23:42:03 +0900

    mosaic program for face and license plate

--- mosaic.py
+++ mosaic.py
@@ -1,23 +1,22 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import imutils
-import easyocr
 
-def detect_LP(img_path):
-    img = cv2.imread(img_path)
+def detectFace(img):
+    cascPath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+
+    return faces
+
+def detect_LP(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    '''
-    필터링, edge detection
-    '''
     bfilter = cv2.bilateralFilter(gray, 11, 11, 17)
     edged = cv2.Canny(bfilter, 30, 200)
-    # plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
 
-    '''
-    Contours 찾기
-    '''
     keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(keypoints)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
@@ -29,70 +28,47 @@ def detect_LP(img_path):
             location = approx
             break
 
-    '''
-    빈 마스크 생성 후 contours 그리기
-    '''
     mask = np.zeros(gray.shape, np.uint8)
     new_image = cv2.drawContours(mask, [location], 0, 255, -1)
     new_image = cv2.bitwise_and(img, img, mask=mask)
 
-    # 번호판으로 인식된 부분만 크롭
     (x, y) = np.where(mask == 255)
     (x1, y1) = (np.min(x), np.min(y))
     (x2, y2) = (np.max(x), np.max(y))
-    cropped_image = gray[x1:x2 + 3, y1:y2 + 3]
+    license_plate_area = gray[x1:x2 + 3, y1:y2 + 3]
 
-    return img, location
-
-def ocr_image(img):
-    reader = easyocr.Reader(['ko', 'en'])
-    result = reader.readtext(img)
-
-    return result[0][1]
+    return license_plate_area, location
 
 def mosaic(src, ratio):
-    """
-    ### 모자이크 기능
-    :param src: 이미지 소스
-    :param ratio: 모자이크 비율
-    :return: 모자이크가 처리된 이미지
-    """
     small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
     mosaic_img = cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
     return mosaic_img
 
 def mosaic_area(src, x, y, width, height, ratio):
-    """
-    ### 부분 모자이크 기능
-    :param src: 이미지 소스
-    :param x: 가로축 모자이크 시작 범위
-    :param y: 세로축 모자이크 시작 범위
-    :param width: 모자이크 범위 넓이
-    :param height: 모자이크 범위 폭
-    :param ratio: 모자이크 비율
-    :return: 부분 모자이크가 처리된 이미지
-    """
     mosaic_area_img = src.copy()
     mosaic_area_img[y:y + height, x:x + width] = mosaic(mosaic_area_img[y:y + height, x:x + width], ratio)
     return mosaic_area_img
 
 def opencv_img_save(img, save_img_path, save_img_name):
-    """
-    ### 처리 이미지 저장 기능
-    :param img: 저장할 이미지
-    :param save_img_path: 이미지 저장 경로
-    :param save_img_name: 저장할 이미지 명
-    """
     cv2.imwrite(save_img_path + save_img_name, img)
 
 def process_image(input_image_path):
-    img, location = detect_LP(input_image_path)
+    img = cv2.imread(input_image_path)
+
+    # 얼굴과 번호판을 찾기
+    faces = detectFace(img)
+    license_plate_area, location = detect_LP(img)
 
     # 모자이크 비율
     mosaic_ratio = 0.05
 
     # 원본 이미지 로드
     original_image = img.copy()
+
+    # 얼굴이 있는 경우 얼굴 부분 모자이크 처리
+    if len(faces) > 0:
+        for (x, y, w, h) in faces:
+            original_image = mosaic_area(original_image, x, y, w, h, mosaic_ratio)
 
     # 번호판 부분을 검출하고 모자이크 처리
     if location is not None:
@@ -107,6 +83,6 @@ def process_image(input_image_path):
     return output_image_path
 
 if __name__ == "__main__":
-    input_image_path = 'data/car_test.jpeg'
+    input_image_path = 'data/mosaic test.jpg'
     result_image_path = process_image(input_image_path)
     print(f"결과 이미지 저장 완료: {result_image_path}")
